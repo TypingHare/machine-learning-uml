3ac1550a948f220f57aba45d5677c44f0238ee06
James Chen <jameschan312.cn@gmail.com>
2025-02-22 20:52:07 -0500

    hw4

--- homework/4/src/part1.py
+++ homework/4/src/part1.py
@@ -1,7 +1,8 @@
-'''
-This is Homework 4 in COMP4220-Machine Learning 
+"""
+This is Homework 4 in COMP4220-Machine Learning
 University of Massachusetts Lowell
-'''
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
@@ -9,17 +10,26 @@ import functools
 
 
 def create_synthetic_data(add_outliers=False, add_class=False):
-    '''This function creates a synthetic 2D data set for classification.
-    It can be used to add outliers and well as more classes.'''
-    x0 = np.random.normal(size=50).reshape(-1,2)-1
-    x1 = np.random.normal(size=50).reshape(-1,2)+1
+    """This function creates a synthetic 2D data set for classification.
+    It can be used to add outliers and well as more classes."""
+    x0 = np.random.normal(size=50).reshape(-1, 2) - 1
+    x1 = np.random.normal(size=50).reshape(-1, 2) + 1
+
     if add_outliers:
-        x_1 = np.random.normal(size=10).reshape(-1,2)+np.array([5.0, 10.])
-        return np.concatenate([x0, x1, x_1]),np.concatenate([np.zeros(25), np.ones(30)]).astype(int)
+        x2 = np.random.normal(size=10).reshape(-1, 2) + np.array([5.0, 10.0])
+        return np.concatenate([x0, x1, x2]), np.concatenate(
+            [np.zeros(25), np.ones(30)]
+        ).astype(int)
+
     if add_class:
-        x_2 = np.random.normal(size=50).reshape(-1,2) + 3.
-        return np.concatenate([x0,x1,x_2]), np.concatenta([np.zeros(25), np.ones(25), 2+np.zeros(25)]).astype(int)
-    return np.concatenate([x0, x1]), np.concatenate([np.zeros(25), np.ones(25)]).astype(int) 
+        x2 = np.random.normal(size=50).reshape(-1, 2) + 3.0
+        return np.concatenate([x0, x1, x2]), np.concatenate(
+            [np.zeros(25), np.ones(25), 2 + np.zeros(25)]
+        ).astype(int)
+
+    return np.concatenate([x0, x1]), np.concatenate(
+        [np.zeros(25), np.ones(25)]
+    ).astype(int)
 
 
 class PolynomialFeature(object):
@@ -29,12 +39,12 @@ class PolynomialFeature(object):
 
     def transform(self, x):
         if x.ndim == 1:
-            x = x[:, None]  #ensure size (:,1) instead of (:,)
-        x_t = x.transpose() #return the data to its initial shape
+            x = x[:, None]  # ensure size (:,1) instead of (:,)
+        x_t = x.transpose()  # return the data to its initial shape
         features = [np.ones(len(x))]
         for degree in range(1, self.degree + 1):
             for items in itertools.combinations_with_replacement(x_t, degree):
-                features.append(functools.reduce(lambda x,y: x*y, items))
+                features.append(functools.reduce(lambda _x, y: _x * y, items))
         return np.asarray(features).transpose()
 
 
@@ -44,74 +54,103 @@ class Classifier(object):
 
 class LogisticRegression(Classifier):
     def __init__(self):
-        pass
+        self.w = None
+
     @staticmethod
     def _sigmoid(a):
-        #--- your code here ---#
-        # part (a)
-        pass
+        """Sigmoid function is defined as σ(a) = 1 / (1 + e^{-a})."""
+        a = np.clip(a, -1e2, 1e2)
+        return 1 / (1 + np.exp(-a))
 
-    def fit(self, x_train: np.ndarray, y_train: np.ndarray, max_iter: int=100):
-        w = np.zeros(np.size(x_train, 1))
+    def fit(
+        self, x_train: np.ndarray, y_train: np.ndarray, max_iter: int = 100
+    ) -> None:
+        # Add bias term
+        x_train = np.hstack([np.ones((x_train.shape[0], 1)), x_train])
+
+        n_features = x_train.shape[1]
+        w = np.zeros(n_features)
         for _ in range(max_iter):
-            w_prev = np.copy(w)
-            #--- your code here ---#
-            # part (b)
-            y = []
+            # Previous w
+            prev_w = w.copy()
 
-            #--- your code here ---#
-            # part (c)
-            grad = []
+            # Prediction
+            y = self._sigmoid(x_train @ w)
 
-            #--- your code here ---#
-            # part (d)            
-            hessian = []
+            # Gradient of the log-likelihood
+            gradient = x_train.T @ (y - y_train)
 
-            #--- your code here ---#
-            # part (e)            
-            w -= 0
+            # Hessian matrix
+            hessian = x_train.T @ np.diagflat(y * (1 - y)) @ x_train
+
+            # Newton-Raphson update: w = w - H^{-1} * gradient
+            w -= np.linalg.pinv(hessian) @ gradient
+
+            # Check for convergence
+            if np.all(np.abs(w - prev_w) < 1e-6):
+                break
 
         self.w = w
 
-    def proba(self, x: np.ndarray):
-        #--- your code here ---#
-        # part (f)        
-        return 0.0
+    def proba(self, x: np.ndarray) -> np.ndarray:
+        # Add bias term for x
+        x = np.hstack([np.ones((x.shape[0], 1)), x])
+        return self._sigmoid(x @ self.w)
 
-    def classify(self, x: np.ndarray, threshold: float=0.5):
-        #--- your code here ---#
-        # part (g)        
-        return 1
+    def classify(self, x: np.ndarray, threshold: float = 0.5):
+        p = self.proba(x)
+        return np.where(p < threshold, 0, 1)
 
 
+# noinspection PyPep8Naming
 def test_LogisticRegression():
-    #--- your code here ---#
-    # part (h)    
-    x_train, t_train = [], []
+    # part (h)
+    x_train, t_train = create_synthetic_data()
 
-    #--- your code here ---#
-    # part (i)    
-    x1_test, x2_test = [], []
-    x_test = []
+    # part (i)
+    x_min, x_max, y_min, y_max = -5, 15, -5, 15
+    x1_test = np.linspace(x_min, x_max, 100)
+    x2_test = np.linspace(y_min, y_max, 100)
+    xx, yy = np.meshgrid(x1_test, x2_test)
 
-    #--- your code here ---#
-    # part (j)    
-    # don't forget the bias term
+    # These are grid points covering all possible combinations of x and y in
+    # [-5, 15]
+    x_test = np.column_stack((xx.ravel(), yy.ravel()))
+
+    # part (j)
     logistic_regression = LogisticRegression()
+    logistic_regression.fit(x_train, t_train)
 
+    # part (k)
+    y_lr = logistic_regression.classify(x_test)
 
-    #--- your code here ---#
-    # part (k)    
-    y_lr = []
-
-    #--- your code here ---#
     # part (l)
-    plt.scatter(0, 0)
-    plt.xlim(-1, 1)
-    plt.ylim(-1, 1)
+    plt.figure(figsize=(10, 8))
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
     plt.title("Logistic Regression")
-    plt.show()    
+
+    # Plot the training data
+    # Scatter the training samples (class 0 in red; class 1 in blue)
+    plt.scatter(
+        x_train[t_train == 0, 0],
+        x_train[t_train == 0, 1],
+        color="r",
+        label="Class 0 (Train)",
+    )
+    plt.scatter(
+        x_train[t_train == 1, 0],
+        x_train[t_train == 1, 1],
+        color="b",
+        label="Class 1 (Train)",
+    )
+
+    # Plot the contour
+    z = y_lr.reshape(xx.shape)
+    plt.contourf(xx, yy, z, alpha=0.5, cmap="RdBu")
+
+    plt.show()
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     test_LogisticRegression()
--- homework/4/src/part2.py
+++ homework/4/src/part2.py
@@ -1,47 +1,35 @@
-import numpy as np
-# part (a) -- import load_wine from scikit-learn
-# part (c) -- import MinMaxScalar
-# part (d) -- import train_test_split
-# part (e) -- import LogisticRegression
-# part (g) -- import classification_report
+from sklearn.datasets import load_wine
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
-
-#--- Your code here ---#
 # part (a)
-data = {}
-X = []
-t = []
+# print(wine.DESCR)  # 178 samples, 13 features
+wine = load_wine()
+X = wine.data
+t = wine.target
 
-
-#--- Your code here ---#
 # part (b)
-print(f'Class names for index 13:')
+target_labels = wine.target_names
+print(f"Class names for index 13: {target_labels[t[13]]}")
+print(f"Class names for index 133: {target_labels[t[133]]}")
 
-
-#--- Your code here ---#
 # part (c)
-X_sc = []
+X_sc = MinMaxScaler().fit_transform(X)
 
-
-#--- Your code here ---#
 # part (d)
-X_train, X_test, t_train, t_test = [], [], [], []
+X_train, X_test, t_train, t_test = train_test_split(X_sc, t, test_size=0.2)
 
-
-#--- Your code here ---#
 # part (e)
-# instantial LogisticRegression with proper arguments
-# then fit using the training set
+lr = LogisticRegression()
+lr.fit(X_train, t_train)
 
-
-#--- Your code here ---#
 # part (f)
-y_pred = []
-score = 0.0
-print(f'Score: {score}')
+y_pred = lr.predict(X_test)
+score = lr.score(X_test, t_test)
+print(f"Score: {score}")
 
-
-#--- Your code here ---#
 # part (g)
-print('report')
-
+print("\n[Report]")
+print(classification_report(t_test, y_pred, target_names=target_labels))
